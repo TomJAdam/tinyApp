74215605aea55579c5db9bd8b26b601991e066e8
Tom Adam <tom.adam21@gmail.com>
2020-09-28 22:42:55 +0000

    added urls_new.ejs, form to submit urls

--- express_server.js
+++ express_server.js
@@ -26,14 +26,18 @@ app.listen(PORT, () => {
 
 //pages
 
+
+
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
-
-
