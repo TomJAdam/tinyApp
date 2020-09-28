6e154e21048c1c11415ae870c5c2504889d2fc50
Tom Adam <tom.adam21@gmail.com>
2020-09-28 16:13:33 +0000

    added bootstrap template for urls_index.ejs

--- express_server.js
+++ express_server.js
@@ -19,10 +19,20 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
 
-app.get('/urls.json', (req, res) => {
-  res.json(urlDatabase);
+// app.get('/urls.json', (req, res) => {
+//   res.json(urlDatabase);
+// });
+
+// app.get('/hello', (req, res) => {
+//   res.send('<html><body>HELLO <b>WORLD</b></body></html>\n');
+// });
+
+app.get('/urls', (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars);
 });
 
 app.get('/hello', (req, res) => {
-  res.send('<html><body>HELLO <b>WORLD</b></body></html>\n');
+  const templateVars = { greeting: 'Hello World!' };
+  res.render('hello_world', templateVars);
 });
