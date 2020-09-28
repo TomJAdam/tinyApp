0d8c795d7e7f43e38a7fee855f742849e3140279
Tom Adam <tom.adam21@gmail.com>
2020-09-28 15:24:03 +0000

    http server running on port 8080

---
+++ express_server.js
@@ -0,0 +1,18 @@
+// express server
+
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+const urlDatabase = {
+  'b2xvn2': 'http://www.lighthouselabs.ca',
+  '9sm5xk': 'http://www.google.com'
+};
+
+app.get('/', (req, res) => {
+  res.send('Hello!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
