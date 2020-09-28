e323fb86309f63feee89d6f03c8a43dbd7ac9055
Tom Adam <tom.adam21@gmail.com>
2020-09-28 23:10:36 +0000

    added randomString funcion

--- express_server.js
+++ express_server.js
@@ -6,6 +6,10 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+
 app.set('view engine', 'ejs');
 
 const urlDatabase = {
@@ -26,8 +30,6 @@ app.listen(PORT, () => {
 
 //pages
 
-
-
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
@@ -41,3 +43,12 @@ app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
+
+app.post("urls", (req, res) => {
+  console.log(req.body)
+  res.send('OK');
+});
+
+const generateRandomString = () => {
+  return Math.random().toString(36).replace(/[^a-z0-9]+/g, '').substr(0, 6);
+};
