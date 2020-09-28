9d4db3b990a1147543f778c2b7efada6234b30c2
Tom Adam <tom.adam21@gmail.com>
2020-09-28 21:23:03 +0000

    added shortURL page, and updated express_server.js

--- express_server.js
+++ express_server.js
@@ -24,20 +24,14 @@ app.listen(PORT, () => {
 
 //pages
 
-// app.get('/urls.json', (req, res) => {
-//   res.json(urlDatabase);
-// });
-
-// app.get('/hello', (req, res) => {
-//   res.send('<html><body>HELLO <b>WORLD</b></body></html>\n');
-// });
-
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
 });
 
-app.get('/hello', (req, res) => {
-  const templateVars = { greeting: 'Hello World!' };
-  res.render('hello_world', templateVars);
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
 });
+
+
